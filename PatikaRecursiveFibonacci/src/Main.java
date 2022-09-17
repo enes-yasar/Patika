f07e8c6abca9cb90457a6346e3c51574fe811e46
Enes <Enes@DESKTOP-ETS4I5Q>
2022-09-17 13:39:50 +0300

    Recursive Fibonacci

---
+++ PatikaRecursiveFibonacci/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static int fibonacci(int x) {
+		if (x == 1 || x == 2) {
+			return 1;
+		}
+		return fibonacci(x - 1) + fibonacci(x - 2);
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Kacinci elemani istiyorsunuz: " );
+		int x = scan.nextInt();
+		System.out.println(x + ". eleman: " + fibonacci(x));
+	}
+
+}
